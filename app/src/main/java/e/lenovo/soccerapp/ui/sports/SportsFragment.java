bd88c69a2a4e0f38fd62554d9b54f667943846f3
BlueGuardian021 <www.dimitris92@gmail.com>
2021-04-01 22:04:41 +0300

    fix about and sports frags put them into mobile_navigation.xml

---
+++ app/src/main/java/e/lenovo/soccerapp/ui/sports/SportsFragment.java
@@ -0,0 +1,38 @@
+package e.lenovo.soccerapp.ui.sports;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import e.lenovo.soccerapp.R;
+
+public class SportsFragment extends Fragment {
+
+    private SportsViewModel mViewModel;
+
+    public static SportsFragment newInstance() {
+        return new SportsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sports, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SportsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
